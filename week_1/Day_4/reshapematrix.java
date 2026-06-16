6ad4f8161589b660bb1691f86dfa5e23573f457c
amansingh301006-lgtm <amansingh301006@gmail.com>
2026-06-16 17:01:47 +0530

    Create reshapematrix.java

---
+++ week_1/Day_4/reshapematrix.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[][] matrixReshape(int[][] mat, int r, int c) {
+        int m = mat.length;
+        int n = mat[0].length;
+
+        
+        if (m * n != r * c) {
+            return mat;
+        }
+
+        int[][] arr = new int[r][c];
+
+        for (int i = 0; i < m * n; i++) {
+            arr[i / c][i % c] = mat[i / n][i % n];
+        }
+
+        return arr;
+    }
+}

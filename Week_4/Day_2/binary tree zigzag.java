75ceb88d4bca0ed597c0005a678b62918bab7b51
amansingh301006-lgtm <amansingh301006@gmail.com>
2026-06-23 21:29:29 +0530

    Create binary tree zigzag.java

---
+++ Week_4/Day_2/binary tree zigzag.java
@@ -0,0 +1,43 @@
+class Solution {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+
+        if (root == null) {
+            return result;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        boolean leftToRight = true;
+
+        while (!queue.isEmpty()) {
+            int n = queue.size();
+            LinkedList<Integer> level = new LinkedList<>();
+
+            for (int i = 0; i < n; i++) {
+                TreeNode node = queue.poll();
+
+                if (leftToRight) {
+                    level.addLast(node.val);
+                } else {
+                    level.addFirst(node.val);
+                }
+
+                if (node.left != null) {
+                    queue.offer(node.left);
+                    }
+
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+
+            result.add(level);
+            leftToRight = !leftToRight;
+        }
+
+        return result;
+
+    }
+}

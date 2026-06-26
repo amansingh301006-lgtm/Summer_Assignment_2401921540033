0bcdc04e6942eac98abf054a944281b9e5be3aa5
amansingh301006-lgtm <amansingh301006@gmail.com>
2026-06-26 09:16:38 +0530

    Create serialize and deserialize.java

---
+++ Week_4/Day_5/serialize and deserialize.java
@@ -0,0 +1,41 @@
+public class Codec {
+
+    
+    public String serialize(TreeNode root) {
+        StringBuilder sb = new StringBuilder();
+        serializeHelper(root, sb);
+        return sb.toString();
+    }
+
+    private void serializeHelper(TreeNode root, StringBuilder sb) {
+        if (root == null) {
+            sb.append("null,");
+            return;
+        }
+
+        sb.append(root.val).append(",");
+        serializeHelper(root.left, sb);
+        serializeHelper(root.right, sb);
+    }
+
+    
+    public TreeNode deserialize(String data) {
+        Queue<String> queue = new LinkedList<>(Arrays.asList(data.split(",")));
+        return deserializeHelper(queue);
+    }
+
+    private TreeNode deserializeHelper(Queue<String> queue) {
+        String value = queue.poll();
+
+        if (value.equals("null")) {
+            return null;
+        }
+
+        TreeNode root = new TreeNode(Integer.parseInt(value));
+        root.left = deserializeHelper(queue);
+        root.right = deserializeHelper(queue);
+
+        return root;
+    }
+}
+
